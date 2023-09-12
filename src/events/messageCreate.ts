3b7bf944f4b3a306d18dc168a912e6afee48d0bc
emitter-166 <xperimentschaftler@gmail.com>
2023-09-12 16:58:54 +0600

    changed perms

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,7 +5,7 @@ export = (client: Client) => {
     client.on('messageCreate', async (msg) => {
         try{
             if(!(msg.content.toLowerCase() === '!events-ad')) return;
-            if(!msg.member?.permissions.has(PermissionFlagsBits.Administrator) && !msg.member?.roles.cache.has('989322114735693858')) return;
+            if(!msg.member?.permissions.has(PermissionFlagsBits.Administrator) && !msg.member?.roles.cache.has('989322114735693858') && !msg.member?.roles.cache.has('1060904484005498971')) return;
             await events_ad(msg);
         }catch(err){
             console.log("Error on /events/messageCreate.ts");
